263ec8f4018c1a9f673827241fbe92f2e213e948
DICZDC <zaito.zd@gmail.com>
2026-01-30 15:20:32 -0600

    feat: Enhance layout and styling for proyecto_detalles view

--- resources/views/proyecto_detalles.blade.php
+++ resources/views/proyecto_detalles.blade.php
@@ -1,20 +1,27 @@
 <x-app-layout>
-    {{ $id }}
-    <div>
-        <p>
-            Informacion del proyecto
-        </p>
-        <p>
-            Mensaje del gobernador del estado
-        </p>
-        <p>
-            19 de enero del 2026
-        </p>
-        <p>
-            Trabjadores Adscritos al Evento:
-        </p>
-    </div>
+    <div class="flex- flex-col
+                {{-- bg-blue-900 --}}
+                ">
 
+        <div class="px-[5%] py-[2%]">
+            <div class="text-cortvRojoBasico gap-2.5 flex flex-col">
+                <p class="text-5xl">
+                    Informacion del proyecto
+                </p>
+                <p class="text-3xl">
+                    Mensaje del gobernador del estado
+                </p>
+                <p class="text-xl">
+                    19 de enero del 2026
+                </p>
+                <p class="text-3xl">
+                    Trabjadores Adscritos al Evento:
+                </p>
+            </div>
+        </div>
+        
+    </div>
+    
     
     <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
 </x-app-layout>
